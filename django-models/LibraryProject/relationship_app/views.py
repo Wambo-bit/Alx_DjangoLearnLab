a578d61c92313fbcee0cf9a59a12043e35be04f5
Mary <wambuimuthima1@gmail.com>
2025-09-08 00:06:41 +0300

    Fix Django views and URLs for book list and library detail

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library
 
-#Function-based view
-def list_books(request):
-    books = Book.objects.all()  # This is what the checker expects
+# Function-based view for listing all books
+def book_list(request):
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-Based View using DetailView
+# Class-based view for library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
