7a1851290d45ecd7926a47d883407a4d32a95dac
Mary <wambuimuthima1@gmail.com>
2025-09-08 03:26:19 +0300

    modified a file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test, permission_required
 from .models import Book, Library
 from .forms import BookForm
+from django.contrib.auth.decorators import permission_required
 
 # --------------------
 # Basic Views
