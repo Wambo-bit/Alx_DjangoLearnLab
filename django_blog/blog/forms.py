86c55c8df3bcf7663759a849be4359f3cd3685e5
Mary <wambuimuthima1@gmail.com>
2025-10-07 15:56:28 +0300

    A blog post

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,18 +1,18 @@
 from django import forms
-from .models import Comment
-from .models import Post
+from .models import Post, Comment
+from taggit.forms import TagWidget  # ✅ for tagging support
 
+# Form for creating/updating posts
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']  # include tags
+        widgets = {
+            'tags': TagWidget(),  
+        }
+
+# Form for comments
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
-        widgets = {
-            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write your comment...'}),
-        }
-
-class PostForm(forms.ModelForm):
-    tags = forms.CharField(required=False, help_text="Add tags separated by commas")
-
-    class Meta:
-        model = Post
-        fields = ['title', 'content', 'tags']
